cd888978fb20cfb4487bb1f45c3f286bf372f4d8
Alisson Almeida <alissonalmeidac@gmail.com>
2021-08-11 09:19:44 -0300

    projeto java

---
+++ java-vscode/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 10;
+        numero = soma(1, 2);
+        System.out.println("Bom dia vendergas kkkk " + numero);
+    }
+    public static int soma(int num1, int num2){
+        return num1 + num2;
+    }
+}
